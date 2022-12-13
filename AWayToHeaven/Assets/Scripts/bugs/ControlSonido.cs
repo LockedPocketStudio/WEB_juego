16a1f50293c390934f235b0b913e1e37b31a7658
Pedrobt1003 <pedrobt1003@gmail.com>
2022-12-13 16:14:48 +0100

    1.1.0
    Arreglado sonido
    Collider torre colocado correctamente

--- AWayToHeaven/Assets/Scripts/bugs/ControlSonido.cs
+++ AWayToHeaven/Assets/Scripts/bugs/ControlSonido.cs
@@ -7,19 +7,34 @@ public class ControlSonido : MonoBehaviour
 {
     public Slider musica;
     public float volumen;
+    public float volumenEscenas;
     AudioSource audio;
     void Start()
     {
-      volumen = musica.normalizedValue;
-      audio = this.GetComponent<AudioSource>();
+        audio = this.GetComponent<AudioSource>();
 
+        if(musica != null)
+        {
+            musica.value = audio.volume = PlayerPrefs.GetFloat("Volumen");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        volumen = musica.normalizedValue;
-        audio.volume = musica.normalizedValue;
-        PlayerPrefs.SetFloat("Volumen", volumen);
+        if(musica != null)
+        {
+            volumen = musica.normalizedValue;
+            PlayerPrefs.SetFloat("Volumen", volumen);
+
+            audio.volume = musica.normalizedValue;
+        }
+       // else
+        //{
+            audio.volume = PlayerPrefs.GetFloat("Volumen");
+        //}
+        
+       
+       volumenEscenas = PlayerPrefs.GetFloat("Volumen");
     }
 }
